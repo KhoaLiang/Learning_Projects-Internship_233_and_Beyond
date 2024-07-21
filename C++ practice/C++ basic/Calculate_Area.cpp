839678d3bb596f42ceddedc81f572b96c6f5c3a8
KhoaLiang <akhoa2020@gmail.com>
2024-07-21 23:05:19 +0700

    yes c++

---
+++ C++ practice/C++ basic/Calculate_Area.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	float a = 7.5, b = 3.8;
+    cout << "Area = " << a * b;
+	return 0;
+}
---
+++ C++ practice/C++ basic/Print_D.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	char d = 'a' + 3;
+	cout << d;
+	return 0;
+}
