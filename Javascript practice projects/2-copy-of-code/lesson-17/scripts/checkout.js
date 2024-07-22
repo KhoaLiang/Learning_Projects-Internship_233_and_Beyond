0fbc30daa1d106aac1eb8f932d05a1dbdeecc158
KhoaLiang <akhoa2020@gmail.com>
2024-07-22 15:10:00 +0700

    rock-paper-scissor game

--- Javascript practice projects/2-copy-of-code/lesson-17/scripts/checkout.js
+++
@@ -1,6 +0,0 @@
-import {renderOrderSummary} from './checkout/orderSummary.js';
-import {renderPaymentSummary} from './checkout/paymentSummary.js';
-// import '../data/cart-class.js';
-
-renderOrderSummary();
-renderPaymentSummary();
