7d35abcbc12718c5741f7ba0f4997a4004ea22cd
KhoaLiang <akhoa2020@gmail.com>
2024-07-24 11:13:04 +0700

    initial look at the project

---
+++ Javascript practice projects/Amazon_clone/Scripts/Amazon.js
@@ -0,0 +1,62 @@
+// Check if the file is connected
+console.log("Amazon.js is connected");
+
+//Products import
+//There isn't any because the products array are in the Amazon.html file
+
+// How for each works is it takes each object and put it in the parameter called products and ran the function linearly
+let productsHTML = '';
+
+products.forEach((products) => {
+    productsHTML += `
+        <div class="product-container">
+            <div class="product-image-container">
+            <img class="product-image"
+                src="${products.image}">
+            </div>
+
+            <div class="product-name limit-text-to-2-lines">
+            ${products.name}
+            </div>
+
+            <div class="product-rating-container">
+            <img class="product-rating-stars"
+                src="images/ratings/rating-${products.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+                ${products.rating.count}
+            </div>
+            </div>
+
+            <div class="product-price">
+            $${(products.priceCents / 100).toFixed(2)}
+            </div>
+
+            <div class="product-quantity-container">
+            <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+            </div>
+
+            <button class="add-to-cart-button button-primary">
+            Add to Cart
+            </button>
+        </div>
+    `;
+})
+document.querySelector('.js-product-grid').innerHTML = productsHTML;
