c1256a9b33dc0cd334764da51fc3e4d5d195f047
KhoaLiang <akhoa2020@gmail.com>
2024-07-24 16:22:13 +0700

    working on checkout page

--- Javascript practice projects/Amazon_clone/data/cart.js
+++ Javascript practice projects/Amazon_clone/data/cart.js
@@ -1,4 +1,10 @@
-export const cart =[];
+export let cart =[{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+}, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+}];
 
 //add too cart function
 export function AddToCart(productId){
@@ -39,4 +45,15 @@ export function UpdateCartQuantity(){
     });
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
+// Delete item from cart
+export function DeleteItemFromCart(productId){
+    const newCart = []; 
+    cart.forEach((item) => {
+        if(item.productId !== productId){
+            newCart.push(item);
+        }
+    });
+    cart = newCart;
 }
