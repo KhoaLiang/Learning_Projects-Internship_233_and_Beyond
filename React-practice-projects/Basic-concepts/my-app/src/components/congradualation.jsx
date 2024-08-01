dd1f57907dc6d81fee54ce76302c12790336379c
KhoaLiang <akhoa2020@gmail.com>
2024-08-01 17:08:20 +0700

    add some code for the build

---
+++ React-practice-projects/Basic-concepts/my-app/src/components/congradualation.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Congratulation = () => {
+    return (
+        <div className='container'>
+            <img src="https://scontent.fsgn8-3.fna.fbcdn.net/v/t39.30808-1/414743475_1041600993784369_6258375353898249420_n.jpg?stp=dst-jpg_p200x200&_nc_cat=111&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=3bGLEIXgb70Q7kNvgHtWY74&_nc_ht=scontent.fsgn8-3.fna&oh=00_AYDz-gwuBg5xNV-PgRMXCJEXlNBk4SIgeX3NasAp_F5r4w&oe=66B11BCE" alt="Congratulatory Image" />
+            <h1>Congratulations!</h1>
+            <p>You will soon build a site like this xD</p>
+            <p>I made this site at 16:05pm 1rst/August/2024</p>
+            <p>I deployd this website at 16:55pm 1rst/August/2024</p>
+        </div>
+    );
+};
+
+export default Congratulation;
--- React-practice-projects/Basic-concepts/my-app/src/index.js
+++ React-practice-projects/Basic-concepts/my-app/src/index.js
@@ -8,6 +8,7 @@ import UseState_Example_Gallery from './components/useState_example.jsx';
 import UseState_Example2_Form from './components/useState_example2.jsx';
 import UseReducer_Example_Counter from './components/useReducer_example.jsx';
 import BookList from './components/bookList.jsx';
+import Congratulation from './components/congradualation.jsx';
 import Props_Example from './components/props_example.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -24,6 +25,7 @@ root.render(
         <Route path="/UseReducer_Example_Counter" element={<UseReducer_Example_Counter />} />
         <Route path="/BookList" element={<BookList />} />
         <Route path="/Props_Example" element={<Props_Example />} />
+        <Route path="/Congratulation" element={<Congratulation />} />
       </Routes>
     </Router>
   </React.StrictMode>
