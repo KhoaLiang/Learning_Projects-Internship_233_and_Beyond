ac8e927ad4625b5b707eedf046928ffc00e22dd1
KhoaLiang <akhoa2020@gmail.com>
2024-08-02 15:29:00 +0700

    useEffect basic example

--- React-practice-projects/react-advanced/src/index.js
+++ React-practice-projects/react-advanced/src/index.js
@@ -8,6 +8,7 @@ import UseState2 from './tutorial/1-useState/final/2-useState-basics';
 import UseState3 from './tutorial/1-useState/final/3-useState-array';
 import UseState4 from './tutorial/1-useState/final/4-useState-object';
 import UseState5 from './tutorial/1-useState/final/5-useState-counter';
+import UseEffect1 from './tutorial/2-useEffect/setup/1-useEffect-basics';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,6 +22,7 @@ root.render(
           <Route path="/useState3" element={<UseState3 />} />
           <Route path="/useState4" element={<UseState4 />} />
           <Route path="/useState5" element={<UseState5 />} />
+          <Route path="/useEffect1" element={<UseEffect1 />} />
         </Routes>
       </Router>
     </React.StrictMode>
--- React-practice-projects/react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ React-practice-projects/react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,33 @@
 import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
+//BUTTTTTTTTTTTTTT if you want to run useEffect only once, you can pass an empty array as the second parameter for example:
+// useEffect(() => {
+//   console.log('call useEffect');
+// }, []);
+// second parameter is an array of dependencies
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  //even though you can't put useEffect inside a condition, you can put a condition inside useEffect!
+  //this useEffect will run every time the value changes because we have [value] as the second parameter
+  useEffect(() => {
+    console.log('useEffect has ran: ' , value + 1, 'times');
+    if(value >= 1) {
+      document.title = `New Messages(${value})`;
+    }
+  }, [value]);
+  const inCreaseValue = () => {
+    setValue((prevState) => {
+      return prevState + 1
+    });
+  }
+  return (
+    <React.Fragment>
+      <h2>{value}</h2>
+      <button className='btn' onClick={() =>  inCreaseValue()}>Click now</button>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectBasics;
