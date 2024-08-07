63c0047d8f41608224699b2da9c1132b1622c01b
KhoaLiang <akhoa2020@gmail.com>
2024-08-08 02:11:30 +0700

    haha context go burrrrrrrr

--- React-practice-projects/react-projects-master/02-tours/src/App.js
+++ React-practice-projects/react-projects-master/02-tours/src/App.js
@@ -1,11 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+import React, { useState, useEffect, createContext } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
+const url = 'http://localhost:3000/tours.json';
+
+// Create a context
+export const ToursContext = createContext();
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+  const [error, setError] = useState(null);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      setLoading(false);
+      console.log("tour:", tours);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (error) {
+    return (
+      <main>
+        <h2>Error: {error}</h2>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <ToursContext.Provider value={{ setTours, tours }}>
+        <Tours tours={tours} />
+      </ToursContext.Provider>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- React-practice-projects/react-projects-master/02-tours/src/Tour.js
+++ React-practice-projects/react-projects-master/02-tours/src/Tour.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, {useContext} from 'react';
+import { ToursContext } from './App';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id, image, info, price, name}) => {
+  const { setTours, tours } = useContext(ToursContext);
+  const removeItem = (id) => {
+    let newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+  return (<article className='single-tour'>
+    <img src={image} alt={name} />
+    <footer>
+      <div className='tour-info'>
+        <h4>{name}</h4>
+        <h4 className='tour-price'>${price}</h4>
+      </div>
+      <p>{info}</p>
+      <button className='delete-btn' onClick={() => removeItem(id)}>not interested</button>
+    </footer>
+  </article>);
 };
 
 export default Tour;
