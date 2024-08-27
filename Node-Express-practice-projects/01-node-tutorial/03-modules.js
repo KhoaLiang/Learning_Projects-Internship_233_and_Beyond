af02217e8864472647d61eec99bdc07dc100c17f
KhoaLiang <akhoa2020@gmail.com>
2024-08-27 11:48:21 +0700

    proving that function can be written in 2 ways

--- Node-Express-practice-projects/01-node-tutorial/03-modules.js
+++ Node-Express-practice-projects/01-node-tutorial/03-modules.js
@@ -3,6 +3,7 @@
 const names = require('./04-names')
 const sayHi = require('./05-utils')
 const data = require('./06-alternative-flavor')
+console.log(data)
 require('./07-mind-grenade')
 sayHi('susan')
 sayHi(names.john)
--- Node-Express-practice-projects/01-node-tutorial/07-mind-grenade.js
+++ Node-Express-practice-projects/01-node-tutorial/07-mind-grenade.js
@@ -1,8 +1,9 @@
 const num1 = 5
 const num2 = 10
-
-function addValues() {
+const addValues = () => {
   console.log(`the sum is : ${num1 + num2}`)
 }
-
+// function addValues() {
+//   console.log(`the sum is : ${num1 + num2}`)
+// }
 addValues()
